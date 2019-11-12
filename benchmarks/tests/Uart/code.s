be4c1d3f1c382f9ecf5d2ea9f9e3cc4377498781
Junichiro Kadomoto <kadomoto@mtl.t.u-tokyo.ac.jp>
2019-11-12 19:04:25 +0900

    Upload New File

---
+++ benchmarks/tests/Uart/code.s
@@ -0,0 +1,18 @@
+    .file    "code.s"
+    .option nopic
+    .text
+    .align    2
+    .globl    main
+    .type     main, @function
+main:
+    li      a0, 0xF6FFF070
+    li      a1, 0x00000041
+    j       main2
+end:
+    li      a7, 1
+end2:
+    j       end2
+main2:
+    sw      a1, 0(a0)            # 0xF6FFF070に0x01000001をストアする
+main3:
+    j       main3                # ここでループして終了
