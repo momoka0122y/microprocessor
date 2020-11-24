72858bc78ef69ec74acb17da6092fadaf1a99941
山本桃歌 <momoka@my.local>
2020-11-24 18:27:56 +0900

    change decoder OPIMM

--- benchmarks/source/decoder.v
+++ benchmarks/source/decoder.v
@@ -27,15 +27,6 @@ always @(ir) begin
 
     case(ir[6:0]) // opcode  //9 cases
     `OPIMM:begin  // imm, rs1, type, rd, case
-        test(name, ir, ex_srcreg1_num, ex_srcreg2_num, ex_dstreg_num, ex_imm, ex_alucode, ex_aluop1_type, ex_aluop2_type, ex_reg_we, ex_is_load, ex_is_store, ex_is_halt)
-        ir = 32'hfff00513; #10;
-        `test("ADDI", ir, 0, 0, 10, 32'hffffffff, `ALU_ADD, `OP_TYPE_REG, `OP_TYPE_IMM, `ENABLE, `DISABLE, `DISABLE, `DISABLE)
-        
-        ir = 32'h5a613; #10;
-        `test("SLTI", ir, 11, 0, 12, 0, `ALU_SLT, `OP_TYPE_REG, `OP_TYPE_IMM, `ENABLE, `DISABLE, `DISABLE, `DISABLE)
-
-        ir = 32'h279793; #10;
-        , , , )
 
         // srcreg1_num
         srcreg1_num <= ir[19:15];
@@ -52,15 +43,14 @@ always @(ir) begin
 
         // alucode
         case(ir[14:12])
-            `OP_ADDi : alucode<=`ALU_ADD;
-            `OP_SLTi : alucode<=`ALU_SLT;
-            `OP_SLTiu : alucode<=`ALU_SLTU;
-            `OP_XORi : alucode <= `ALU_XOR;
-            `OP_ORi : alucode <= `ALU_OR;
-            `OP_ANDi :alucode <= `ALU_AND;
-            `OP_SLLi :alucode <= `ALU_SLL;
-            `OP_SRi :alucode <= ir[30]?`ALU_SRA:`ALU_SRL;
-            default:alucode <=`UNUSED;
+            3'b000 : alucode <=`ALU_ADD;
+            3'b010 : alucode <=`ALU_SLT;
+            3'b011 : alucode <=`ALU_SLTU;
+            3'b100 : alucode <= `ALU_XOR;
+            3'b110 : alucode <= `ALU_OR;
+            3'b111 :alucode <= `ALU_AND;
+            3'b001 :alucode <= `ALU_SLL;
+            3'b101 :alucode <= ir[30]?`ALU_SRA:`ALU_SRL;
         endcase
         // aluop1_type
         aluop1_type <= `OP_TYPE_REG
