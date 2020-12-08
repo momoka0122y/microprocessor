d7e643a451006ce602033cc01e0023f4df4564ca
momoka0122y <momoka.my6@gmail.com>
2020-12-08 14:53:03 +0900

    fix warnings

--- source/decoder.v
+++ source/decoder.v
@@ -152,7 +152,7 @@ always @(ir) begin
 
       `JALR  : begin
         op_type <= `TYPE_I;
-        reg_we <= (ir[11:7] ß== 0) ?  `ENABLE : `DISABLE;
+        reg_we <= (ir[11:7] == 0) ?  `ENABLE : `DISABLE;
         alucode <= `ALU_JALR
         aluop1_type <= `OP_TYPE_REG;
         aluop2_type <= `OP_TYPE_PC;
